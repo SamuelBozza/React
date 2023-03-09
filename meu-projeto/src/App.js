1ab7a127bd1c6fbdb147a33c4891f0237be76a52
Samuel Bozza <102820398+SamuelBozza@users.noreply.github.com>
2023-03-09 07:36:56 -0300

    Att

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  const name = 'Samuel'
+
+  const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
+  return (
+    <div className="App">
+      <h2>Alterando JSX</h2>
+      <p>Ola, {name}</p>
+      <p>Soma: {sum(1, 2)}</p>
+      <img src={url} alt='Minha Imagem'></img>
+    </div>
+  );
+}
+
+export default App;
