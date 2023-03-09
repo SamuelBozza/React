96700c10d1b9cb7fd9450c99f1ddf7af701a3069
Samuel Bozza <102820398+SamuelBozza@users.noreply.github.com>
2023-03-09 10:56:46 -0300

    att

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,22 +1,17 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const name = 'Samuel'
-
-  const newName = name.toUpperCase()
-
-  function sum(a, b) {
-    return a + b
-  }
-
-  const url = 'https://via.placeholder.com/150'
-
+  const nome = 'Samuel'
   return (
     <div className="App">
-      <h2>Alterando JSX</h2>
-      <p>Ola, {name}</p>
-      <p>Soma: {sum(1, 2)}</p>
-      <img src={url} alt='Minha Imagem'></img>
+      <SayMyName nome="Matheus" />
+      <SayMyName nome={nome} />
+      <Pessoa
+      nome='Rodrigo'
+      idade='28' profissao='Programador' foto='https://via.placeholder.com/150'/>
     </div>
   );
 }
